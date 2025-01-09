234a0300605277dcdca247331beb2a073f98049d
Nazar Hylov <Gilov0001@gmail.com>
2025-01-10 00:11:16 +0100

    10.01 Homework

--- homework.js
+++ homework.js
@@ -1,43 +1,47 @@
-/*Вам необхідно написати програму, яка приймає на вхід число і виводить у консоль повідомлення у форматі Вам N рік / роки / років. Залежно від числа N слово рік має змінюватися на років або року. Наприклад, Вам 1 рік, Вам 5 років, Вам 42 роки.*/
 
-const yourAge = +prompt('Сколько вам лет?');
-const secondTypeOfYears = [2,3,4]
-if (!isNaN(yourAge) && yourAge > 0) {
-    if (yourAge === 1 || (yourAge % 10 === 1 && yourAge % 100 !== 11)) {
-        console.log('Вам ' + yourAge + ' год');
-    } else if (secondTypeOfYears.includes(yourAge % 10) && (yourAge % 100 < 11 || yourAge % 100 > 14)) {
-        console.log('Вам ' + yourAge + ' года');
-    } else {
-        console.log('Вам ' + yourAge + ' лет');
+/*Вам необхідно написати програму, яка приймає на вхід число і виводить у консоль повідомлення у форматі: Число secondNumber є простим числом, якщо число secondNumber просте, та Число secondNumber не є простим числом, якщо число secondNumber складене.*/
+
+let num = +prompt("Enter a number");
+
+if (num > 1) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            console.log(`Число ${num} не є простим числом`);
+            break;
+        }
+        if (i === num - 1) {
+            console.log(`Число ${num} є простим числом`);
+        }
     }
 } else {
-    console.log('Введите корректное число');
+    console.log(`Число ${num} не є простим числом`);
 }
 
-/*Вам необхідно написати програму, яка приймає на вхід число і виводить у консоль повідомлення, що вказує, чи є введений рік високосним.*/
+/* Вам необхідно написати програму, яка приймає на вхід число N і знаходить усі числа в діапазоні від 1 до N, які є досконалими числами.*/
 
-const year = +prompt('Какой год?');
-if (!isNaN(year) && year > 0) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-        console.log('Год ' + year + ' является високосным');
-    } else {
-        console.log('Год ' + year + ' является невисокосным');
+let secondNumber = +prompt("Enter a number");
+
+for (let num = 1; num <= secondNumber; num++) {
+    let sum = 0;
+
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
+            sum += i;
+        }
     }
-} else {
-    console.log('Введите корректное число');
+
+    if (sum === num) {
+        console.log(`Число ${num} є досконалим числом`);
+    }
 }
 
-/*Вам необхідно написати програму, яка приймає на вхід число і виводить у консоль повідомлення залежно від значення числа. Якщо число ділиться на 3, то повідомлення має бути Fizz, якщо число ділиться на 5, то повідомлення має бути Buzz, а якщо число ділиться і на 3, і на 5, то повідомлення має бути FizzBuzz. Наприклад, для числа 15 повідомлення має бути FizzBuzz.*/
 
-const someNumber = +prompt('Какое число?');
-if (!isNaN(someNumber) && someNumber > 0) {
-    if (someNumber % 3 === 0 && someNumber % 5 === 0) {
-        console.log('FizzBuzz')
-    } else if (someNumber % 3 === 0) {
-        console.log('Fizz')
-    } else if (someNumber % 5 === 0) {
-        console.log('Buzz')
-    }
-} else {
-    console.log('Введите корректное число');
+ /*Вам необхідно написати програму, яка приймає на вхід число, що буде висотою вершини вашої ялинки. Уся ялинка має бути реалізована одним рядком:*/
+
+let height = +prompt("Enter the height of the tree");
+
+for (let i = 1; i <= height; i++) {
+    let spaces = " ".repeat(height - i);
+    let stars = "*".repeat(2 * i - 1);
+    console.log(spaces + stars);
 }
