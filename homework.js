2105abd11dff2e5bf936d0da59cc34b8d4c8eccb
Nazar Hylov <Gilov0001@gmail.com>
2025-01-16 17:43:18 +0100

    16.01 Homework

--- homework.js
+++ homework.js
@@ -1,102 +1,37 @@
+//Вам необхідно написати функцію doubleLetter(str), яка приймає на вхід рядок і повертає новий рядок, у якому кожен символ повторюється двічі hello ⇒ hheelllloo.
 
-/* Вам необхідно написати програму, яка приймає на вхід число і виводить у консоль повідомлення у форматі: Число secondNumber є простим числом, якщо число secondNumber просте, та Число secondNumber не є простим числом, якщо число secondNumber складене.*/
-
-let num = +prompt("Enter a number");
-
-if (num > 1) {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            console.log(`Число ${num} не є простим числом`);
-            break;
-        }
-        if (i === num - 1) {
-            console.log(`Число ${num} є простим числом`);
-        }
+function doubleLetter(str) {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        result += str[i] + str[i]; // Добавляем каждый символ дважды
     }
-} else {
-    console.log(`Число ${num} не є простим числом`);
+    return result;
 }
 
-/* Вам необхідно написати програму, яка приймає на вхід число N і знаходить усі числа в діапазоні від 1 до N, які є досконалими числами.*/
+//Вам необхідно написати функцію padString(str, length, symbol, toLeft), яка приймає на вхід рядок, число, що є довгим рядком, який ми хочемо отримати в результаті та символ, яким доповниться рядок, якщо це буде потрібно, четвертим параметром є буремний «прапор», чи додавати символи зліва або справа(за замовчуванням). Якщо 2 параметр менше, ніж довжина вихідного рядка, то виводимо вихідний рядок без змін. Приклад виклику: padString('Ivan', 6, '*') // 'Ivan**'.
 
-let secondNumber = +prompt("Enter a number");
-
-for (let num = 1; num <= secondNumber; num++) {
-    let sum = 0;
-
-    for (let i = 1; i < num; i++) {
-        if (num % i === 0) {
-            sum += i;
-        }
+function padString(str, length, symbol, toLeft = false) {
+    if (str.length >= length) {
+        return str;
     }
-
-    if (sum === num) {
-        console.log(`Число ${num} є досконалим числом`);
+    const paddingLength = length - str.length;
+    const padding = symbol.repeat(paddingLength);
+    if (toLeft) {
+        return padding + str;
+    } else {
+        return str + padding;
     }
 }
 
+//Вам необхідно написати функцію camelCase(str, separator), яка приймає на вхід рядок і перетворює його до формату camelCase.
 
- /*Вам необхідно написати програму, яка приймає на вхід число, що буде висотою вершини вашої ялинки. Уся ялинка має бути реалізована одним рядком:*/
-
-let height = +prompt("Enter the height of the tree");
-
-for (let i = 1; i <= height; i++) {
-    let spaces = " ".repeat(height - i);
-    let stars = "*".repeat(2 * i - 1);
-    console.log(spaces + stars);
-}
-
-//==============================================================================================================
-////============================= Следующая домашка
-//==============================================================================================================
-
-// Вам необхідно написати функцію reverseString(str), яка приймає на вхід рядок і повертає його у зворотному порядку.
-
-function reverseString(str) {
-    let reversed = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return reversed;
-}
-
-
-// Вам необхідно написати функцію isPalindrome(str), яка приймає на вхід рядок і перевіряє, чи є введений рядок паліндромом.
-
-function isPalindrome(str) {
-    let reversed = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return str === reversed;
-}
-
-// Вам необхідно написати функцію findGCD(a, b), яка приймає на вхід два числа і повертає їхній НСД.
-
-function findGCD(a, b) {
-    // Знаходимо дільники числа a
-    let divisorsA = [];
-    for (let i = 1; i <= a; i++) {
-        if (a % i === 0) {
-            divisorsA.push(i);
+function camelCase(str, separator) {
+    const words = str.split(separator);
+    const camelCased = words.map((word, index) => {
+        if (index === 0) {
+            return word.toLowerCase();
         }
-    }
-
-    // Знаходимо дільники числа b
-    let divisorsB = [];
-    for (let i = 1; i <= b; i++) {
-        if (b % i === 0) {
-            divisorsB.push(i);
-        }
-    }
-
-    // Знаходимо найбільший спільний дільник
-    let gcd = 1;
-    for (let i = 0; i < divisorsA.length; i++) {
-        if (divisorsB.includes(divisorsA[i])) {
-            gcd = divisorsA[i];
-        }
-    }
-
-    return gcd;
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+    return camelCased.join('');
 }
